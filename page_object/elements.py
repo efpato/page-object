605205951011ea3645b0344a31241f0b79254d4d
Sergey Demenok <sergey.demenok@gmail.com>
2015-10-19 22:12:17 +0300

    change Textbox/Select setters

--- page_object/elements.py
+++ page_object/elements.py
@@ -1,8 +1,6 @@
 # -*- coding: -utf-8 -*-
 
-from page_object import PageElement
-
-from selenium.webdriver.support.wait import WebDriverWait
+from page_object import PageElement, PageElementError
 
 
 class CheckableElement(PageElement):
@@ -13,31 +11,40 @@ class CheckableElement(PageElement):
                 element.click()
 
 
-class Checkbox(CheckableElement): pass
+class Checkbox(CheckableElement):
+    pass
 
 
-class Radio(CheckableElement): pass
+class Radio(CheckableElement):
+    pass
 
 
-class Button(PageElement): pass
+class Button(PageElement):
+    pass
 
 
-class Link(PageElement): pass
+class Link(PageElement):
+    pass
 
 
 class Select(PageElement):
     def __set__(self, instance, value):
         if value is not None:
-            value = str(value)
+            value = str(value).strip()
+            element = self.__get__(instance, instance.__class__)
             if len(value) > 0:
-                WebDriverWait(instance.webdriver, self.TIMEOUT).until(
-                    lambda d: self.find(d).find_element_by_xpath("./option[text()='%s']" % value),
-                    "Didn\'t find option by text='%s'." % value).click()
+                for option in element.find_elements_by_xpath("./option"):
+                    if option.text.strip() == value:
+                        option.click()
+                        return 1
+                raise PageElementError("""Select<"%s"> has no option with text "%s" """ % (self._locator[1], value))
 
 
 class Textbox(PageElement):
     def __set__(self, instance, value):
         if value is not None:
+            element = self.__get__(instance, instance.__class__)
             value = str(value)
             if len(value) > 0:
-                self.__get__(instance, instance.__class__).send_keys(value)
+                element.clear()
+                element.send_keys(value)
