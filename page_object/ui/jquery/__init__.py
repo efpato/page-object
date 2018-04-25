ebeda9283930ed2d376039fdf1a9d5bc172b0bf0
Sergey Demenok <sergey.demenok@gmail.com>
2018-04-25 16:23:38 +0300

    add JQuery button

--- page_object/ui/jquery/__init__.py
+++ page_object/ui/jquery/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 
+from page_object.ui.jquery.button import Button, ButtonWrapper, Link
 from page_object.ui.jquery.select import Select, SelectWrapper
 from page_object.ui.jquery.textbox import Textbox, TextboxWrapper
